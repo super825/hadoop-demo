04839eefb8354da206c480144f408ad5f0259172
super825 <106498381@qq.com>
2019-09-16 10:21:08 +0800

    modify content

--- join-demo/src/main/java/com/ada/etl/ETLDriver.java
+++ join-demo/src/main/java/com/ada/etl/ETLDriver.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -12,7 +14,14 @@ import java.io.IOException;
 
 public class ETLDriver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        Job job = Job.getInstance(new Configuration());
+        Configuration configuration = new Configuration();
+
+        // 开启map端输出压缩
+        configuration.setBoolean("mapreduce.map.output.compress", true);
+        // 设置map端输出压缩方式
+        configuration.setClass("mapreduce.map.output.compress.codec", BZip2Codec.class, CompressionCodec.class);
+
+        Job job = Job.getInstance(configuration);
 
         job.setJarByClass(ETLDriver.class);
 
@@ -23,8 +32,18 @@ public class ETLDriver {
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(NullWritable.class);
 
-        FileInputFormat.setInputPaths(job, new Path("d:\\input"));
-        FileOutputFormat.setOutputPath(job, new Path("d:\\output"));
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path("c:\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("c:\\output"));
+
+        // 设置reduce端输出压缩开启
+        FileOutputFormat.setCompressOutput(job, true);
+        // 设置压缩的方式
+        FileOutputFormat.setOutputCompressorClass(job, BZip2Codec.class);
+//	    FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
+//	    FileOutputFormat.setOutputCompressorClass(job, DefaultCodec.class);
 
         boolean b = job.waitForCompletion(true);
         System.exit(b ? 0 : 1);
